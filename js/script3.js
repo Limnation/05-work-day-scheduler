4c1274fb1332423efed094e2e737f5432e7a7ee6
Scott Erwin <scottallenerwin@gmail.com>
2021-03-18 19:17:05 -0400

    messing around with trying to use toarray and a for each instead of writing out the array by hand like i am currently using in script1

--- js/script3.js
+++ js/script3.js
@@ -14,63 +14,72 @@ var hours = [
         stime: "9AM",
         idT: "#t-1",
         tId: "t-1",
-        btn: "b-1"
+        btn: "b-1",
+        btnB: "#b-1"
     },
     {
         time: "10",
         stime: "10AM",
         idT: "#t-2",
         tId: "t-2",
-        btn: "b-2"
+        btn: "b-2",
+        btnB: "#b-2"
     },
     {
         time: "11",
         stime: "11AM",
         idT: "#t-3",
         tId: "t-3",
-        btn: "b-3"
+        btn: "b-3",
+        btnB: "#b-3"
     },
     {
         time: "12",
         stime: "12AM",
         idT: "#t-4",
         tId: "t-4",
-        btn: "b-4"
+        btn: "b-4",
+        btnB: "#b-4"
     },
     {
         time: "13",
         stime: "1pm",
         idT: "#t-5",
         tId: "t-5",
-        btn: "b-5"
+        btn: "b-5",
+        btnB: "#b-5"
     },
     {
         time: "14",
         stime: "2pm",
         idT: "#t-6",
         tId: "t-6",
-        btn: "b-6"
+        btn: "b-6",
+        btnB: "#b-6"
     },
     {
         time: "15",
         stime: "3pm",
         idT: "#t-7",
         tId: "t-7",
-        btn: "b-7"
+        btn: "b-7",
+        btnB: "#b-7"
     },
     {
         time: "16",
         stime: "4pm",
         idT: "#t-8",
         tId: "t-8",
-        btn: "b-8"
+        btn: "b-8",
+        btnB: "#b-8"
     },
     {
         time: "17",
         stime: "5pm",
         idT: "#t-9",
         tId: "t-9",
-        btn: "b-9"
+        btn: "b-9",
+        btnB: "#b-9"
     }
 ];
 
@@ -81,7 +90,7 @@ for (let i = 0; i < hours.length; i++) {
     <div class='input-group-prepend'>
       <span class='input-group-text' id='inputGroup-sizing-default'>${hours[i].stime}</span>
     </div>
-    <textarea id='${hours[i].tId}' type='text' class='form-control' placeholder='Activities' aria-label='Recipient's username' aria-describedby='basic-addon2'></textarea>
+    <textarea id='${hours[i].tId}' data-hourB='${hours[i].btnB}' data-hourT='${hours[i].idT}' type='text' class='form-control area' placeholder='Activities' aria-label='Recipient's username' aria-describedby='basic-addon2'></textarea>
     <button id='${hours[i].btn}' class='btns savedIcon' class='btn btn-outline-secondary' type='button'><img src='images/save.png' alt='save icon'></button>
   </div>`
     containerEl.append(newRow)
@@ -96,11 +105,11 @@ for (let i = 0; i < hours.length; i++) {
         $(idCssT).css("background-color", "#fc4a409c" );
         // $(idCssT).addclass("present"); if i wanted to used the provided classes
     }
-    //changes backgound color to "green" for future time
+    //changes backgound color to "green" for current time
     if (militaryTime < hoursTime) {
         $(idCssT).css("background-color", "#40fc6f9c");
     }
-    //changes backgound color to "gray" for past time
+    //changes backgound color to "gray" for current time
     if (militaryTime > hoursTime) {
         $(idCssT).css("background-color", "#5050509c");
     }
@@ -111,9 +120,17 @@ $(document).ready(function () {
     $('.btns').on("click", function () {
         localStorage.setItem($(this).prop("id"), $(this).prev().val());
     });
-    arr = [{b:'b-1',t:'#t-1'},{b:'b-2',t:'#t-2'},{b:'b-3',t:'#t-3'},{b:'b-4',t:'#t-4'},{b:'b-5',t:'#t-5'},{b:'b-6',t:'#t-6'},{b:'b-7',t:'#t-7'},{b:'b-8',t:'#t-8'},{b:'b-9',t:'#t-9'}]
-    for (let i = 0; i < arr.length; i++) {
-        var savedT = localStorage.getItem(arr[i].b);
-        $(arr[i].t).val(savedT);
+    function localStorage() {
+        var timeareas = $('.area').toArray();
+        console.log(timeareas);
+        timeareas.forEach(element => {
+            var cat = $(element).attr('data-hourB');
+            var dog = $(element).attr('data-hourT');
+            console.log(cat);
+            console.log(dog);
+            // var savedT = localStorage.getItem(arr[i].b);
+            // $(arr[i].t).val(savedT);
+        });
     }
+    localStorage()
 });
