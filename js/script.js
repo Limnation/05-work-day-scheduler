2005977eb5915ad2eff3464b41854011960588e1
Scott Erwin <scottallenerwin@gmail.com>
2021-03-17 22:39:00 -0400

    got the loop in the save data portion by adding another array in that section then calling it also renamed everything so now v3js is sript and v1 is now script2 also switch the index so my index2 is not index so the short version is the main version and the long version is the second index

--- js/script.js
+++ js/script.js
@@ -13,63 +13,63 @@ var hours = [
         stime: "9AM",
         idT: "#t-1",
         tId: "t-1",
-        btn: "#b-1"
+        btn: "b-1"
     },
     {
         time: "10",
         stime: "10AM",
         idT: "#t-2",
         tId: "t-2",
-        btn: "#b-2"
+        btn: "b-2"
     },
     {
         time: "11",
         stime: "11AM",
         idT: "#t-3",
         tId: "t-3",
-        btn: "#b-3"
+        btn: "b-3"
     },
     {
         time: "12",
         stime: "12AM",
         idT: "#t-4",
         tId: "t-4",
-        btn: "#b-4"
+        btn: "b-4"
     },
     {
         time: "13",
         stime: "1pm",
         idT: "#t-5",
         tId: "t-5",
-        btn: "#b-5"
+        btn: "b-5"
     },
     {
         time: "14",
         stime: "2pm",
         idT: "#t-6",
         tId: "t-6",
-        btn: "#b-6"
+        btn: "b-6"
     },
     {
         time: "15",
         stime: "3pm",
         idT: "#t-7",
         tId: "t-7",
-        btn: "#b-7"
+        btn: "b-7"
     },
     {
         time: "16",
         stime: "4pm",
         idT: "#t-8",
         tId: "t-8",
-        btn: "#b-8"
+        btn: "b-8"
     },
     {
         time: "17",
         stime: "5pm",
         idT: "#t-9",
         tId: "t-9",
-        btn: "#b-9"
+        btn: "b-9"
     }
 ];
 
@@ -81,10 +81,8 @@ for (let i = 0; i < hours.length; i++) {
     <div class='input-group-prepend'>
       <span class='input-group-text' id='inputGroup-sizing-default'>${hours[i].stime}</span>
     </div>
-    <textarea id='${hours[i].tId}' data-hour='${hours[i].time}' type='text' class='form-control' placeholder='Activities' aria-label='Recipient's username' aria-describedby='basic-addon2'></textarea>
-    <div class='input-group-append savedIcon'>
-      <button id='${hours[i].btn}' class='btn btn-outline-secondary' type='button'><img src='/images/save.png' alt='saved icon'></button>
-    </div>
+    <textarea id='${hours[i].tId}' type='text' class='textarea form-control' placeholder='Activities' aria-label='Recipient's username' aria-describedby='basic-addon2'></textarea>
+    <button id='${hours[i].btn}' class='btns savedIcon' class='btn btn-outline-secondary' type='button'><img src='/images/save.png' alt='saved icon'></button>
   </div>`
    
     console.log(hours[i])
@@ -110,13 +108,12 @@ for (let i = 0; i < hours.length; i++) {
 }
 
 $(document).ready(function () {
-    $('#t-1').on("click", function () {
-        localStorage.setItem("timeOneText", $('#t-1').val());
-        // var savedT1 = JSON.parse(localStorage.getItem("timeOneText"));
-        // $('#t-1').val(savedT1);
-            // if (t1 === JSON.parse(localStorage.getItem("t-1Text"))) {
-            //     $('#t-1').css("color", "black");
-            //     $('#t-1').css("font-weight", "bold");
-            // }  
+    $('.btns').on("click", function () {
+        localStorage.setItem($(this).prop("id"), $(this).prev().val());
     });
+    arr = [{b:'b-1',t:'#t-1'},{b:'b-2',t:'#t-2'},{b:'b-3',t:'#t-3'},{b:'b-4',t:'#t-4'},{b:'b-5',t:'#t-5'},{b:'b-6',t:'#t-6'},{b:'b-7',t:'#t-7'},{b:'b-8',t:'#t-8'},{b:'b-9',t:'#t-9'}]
+    for (let i = 0; i < arr.length; i++) {
+        var savedT = localStorage.getItem(arr[i].b);
+        $(arr[i].t).val(savedT);
+    }
 });
